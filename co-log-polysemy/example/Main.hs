122df1fde3cb1744344df2e033792000b1b7da0d
Andreas Ländle <andreas.laendle@de.bosch.com>
2021-03-24 06:40:02 +0100

    Establish baseline.

--- co-log-polysemy/example/Main.hs
+++ co-log-polysemy/example/Main.hs
@@ -4,14 +4,19 @@ import Prelude hiding (log)
 
 import Polysemy (Member, Sem, runM)
 
-import Colog.Core.IO (logStringStdout)
+import Colog (Message, richMessageAction, Severity (Debug, Info), Msg(..))
 import Colog.Polysemy (Log, log, runLogAction)
+import Data.Text ( Text )
+import GHC.Stack (HasCallStack, withFrozenCallStack, callStack)
 
+log' :: HasCallStack => Member (Log Message) r => Severity -> Text -> Sem r ()
+log' msgSeverity msgText =
+    withFrozenCallStack (log Msg { msgStack = callStack, .. })
 
-example :: Member (Log String) r => Sem r ()
+example :: Member (Log Message) r => Sem r ()
 example = do
-    log @String "First message..."
-    log @String "Second message..."
+    log' Debug "First message..."
+    log' Info  "Second message..."
 
 main :: IO ()
-main = runM $ runLogAction @IO logStringStdout example
+main = runM $ runLogAction @IO richMessageAction example
